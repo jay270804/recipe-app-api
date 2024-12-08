dc85417c65bd2fe85f323e2c904749d892187435
jay270804 <jaypatel270804@gmail.com>
2024-12-08 16:41:14 +0530

    config/database setup and configuration with django.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """ Adds Two Numbers and returns sum """
+    return x+y
+
+def sub(x,y):
+    """ Subs two numbers and returns positive differnce """
+    return abs(x-y)
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+""" 
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module """
+
+    def test_add_module(self):
+        """ Test adding numbers together """
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_sub_module(self):
+        """ Test substracting numbers together """
+        res=calc.sub(10,15)
+
+        self.assertEqual(res, 5)

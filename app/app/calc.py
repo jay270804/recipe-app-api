c3e279a324bd0ac5831e896f2e66f93143122d0f
jay270804 <jaypatel270804@gmail.com>
2024-12-08 19:37:32 +0530

    feature/added wait_for_db manage command to check database racing.

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """ Adds Two Numbers and returns sum """
     return x+y
 
-def sub(x,y):
+
+def sub(x, y):
     """ Subs two numbers and returns positive differnce """
     return abs(x-y)
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,21 +1,22 @@
-""" 
+"""
 Sample Tests
 """
 
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """ Test the calc module """
 
     def test_add_module(self):
         """ Test adding numbers together """
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_sub_module(self):
         """ Test substracting numbers together """
-        res=calc.sub(10,15)
+        res = calc.sub(10, 15)
 
         self.assertEqual(res, 5)

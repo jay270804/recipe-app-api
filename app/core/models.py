75ce192611734129240a91a800ec980efcbedb56
jay270804 <jaypatel270804@gmail.com>
2024-12-19 23:14:41 +0530

    test/added 2 tests for normalize email and email not empty

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,9 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, Save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address, it cannot be empty.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 

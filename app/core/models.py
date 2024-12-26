c02f86daa42045f35ce43b70c5d144beddf830ee
jay270804 <jaypatel270804@gmail.com>
2024-12-26 14:21:15 +0530

    feature/ added tests for admin and customized admin panel for user.

--- app/core/models.py
+++ app/core/models.py
@@ -16,9 +16,19 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create, Save and return a new user."""
         if not email:
-            raise ValueError('User must have an email address, it cannot be empty.')
+            raise ValueError('User must have an email address, \
+                              it cannot be empty.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, Save and return a superuser"""
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,10 +30,26 @@ class ModelTests(TestCase):
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email=email,password='Sample@123')
+            user = get_user_model().objects.create_user(
+                email=email,
+                password='Sample@123'
+            )
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raise_error(self):
         """Test that creating a user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(email='', password='Sample@123')
+            get_user_model().objects.create_user(
+                email='',
+                password='Sample@123'
+            )
+
+    def test_create_superuser(self):
+        """Test whether superuser is created successfully"""
+        user = get_user_model().objects.create_superuser(
+            email='test@example.com',
+            password='Sample@123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
